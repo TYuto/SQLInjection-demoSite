8e55c950e88444112f29e916570d049ce69fe42a
YutoTamura <yuto.tamur@gmail.com>
2018-12-12 00:25:34 +0900

    [add] これまでに実行したquerrysの表示

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,24 @@
 
 <template>
   <section>
-    <b-card class="">
-      <b-card class="">
-        <p> {{ stdout }} </p>
-        <b-table :items="items"/>
-      </b-card>
+    <b-card class="mx-5 my-5">
+      <h4>sqlの実行</h4>
       <b-input
         v-model="sqlquerry"
         @keydown.enter.native="goquerry"/>
+    </b-card>
+    <b-card class="mx-5 my-5">
+      <h4>実行されたsql</h4>
+      <p 
+        style="white-space: pre;"
+        v-text="querrys"/>
+    </b-card>
+    <b-card class="mx-5 my-5">
+      <h4>tableの表示</h4>
+      <b-input
+        v-model="tablename"
+        @keydown.enter.native="selectall"/>
+      <b-table :items="items"/>
     </b-card>
   </section>
 </template>
@@ -21,7 +31,9 @@ export default {
       sqlquerry: '',
       db: {},
       stdout: '',
-      items: []
+      items: [],
+      querrys: '',
+      tablename: ''
     }
   },
   created: function() {
@@ -29,21 +41,27 @@ export default {
     this.resetdb()
   },
   methods: {
-    goquerry: function(){
-      let querry = this.sqlquerry
+    runquery: function(querry) {
+      this.querrys += querry + "\n\n"
       let res = this.db.exec(querry)
       console.log(res)
-      this.items = res[0].values
-      console.log(this.items)
+      return res
+    },
+    goquerry: function(){
+      let querry = this.sqlquerry
+      let res = this.runquery(querry)
       this.sqlquerry = ''
     },
     resetdb: function() {
-      let sqlstr = "CREATE TABLE users (id int, username char, password char);";
-      sqlstr += "INSERT INTO users VALUES (0, 'admin','passwordhoge');"
+      let sqlstr = "CREATE TABLE users (id int, username char, password char);\n";
+      sqlstr += "INSERT INTO users VALUES (0, 'admin','passwordhoge');\n"
       sqlstr += "INSERT INTO users VALUES (1, 'user1', 'passpass');"
-      this.db.run(sqlstr);
-      let res = this.db.exec("SELECT * FROM users");
-      console.log(res)
+      this.runquery(sqlstr)
+      let res = this.runquery("SELECT * FROM users");
+    },
+    selectall: function(){
+      let res = this.runquery("SELECT * FROM " + this.tablename);
+      this.items = res[0].values
     }
   }
 }
